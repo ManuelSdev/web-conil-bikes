9c434587b940e40e474fac87ede19cd28fe51d2a
Manuel Sánchez <masanchezzm@gmail.com>
2025-04-18 10:11:31 +0200

    eslint add rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
          '@typescript-eslint/no-unused-vars': 'off',
          '@typescript-eslint/ban-ts-comment': 'off',
          'react-hooks/exhaustive-deps': 'off',
+         '@typescript-eslint/no-unused-expressions': 'off',
       },
    },
    /*
